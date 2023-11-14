906dbced36faca10a442bdc4b44c7d8cb781d0c2
Luis <lgmarchioreh@gmail.com>
2023-11-14 11:12:00 -0300

    FEAT(product) Implemented add_product to add a product to a list

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from management.product_handler import get_product_by_id, get_products_by_type
+from management.product_handler import get_product_by_id, get_products_by_type, add_product
+from menu import products
 
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
     #print(get_product_by_id(2500))
-    print(get_products_by_type('BAH'))
+    #print(get_products_by_type('BAH'))
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food"
+    }
+    print(add_product(products, **new_product))
     ...
--- management/product_handler.py
+++ management/product_handler.py
@@ -2,19 +2,25 @@ from menu import products
 
 
 def get_product_by_id(id: int):
-    i = 0
     for product in products:
         if (product["_id"] == id):
             return product
-        i+1
     return {}
 
 
 def get_products_by_type(type: str):
-    i = 0
     products_list = []
     for product in products:
         if (product["type"] == type):
             products_list.append(product)
-        i+1
     return products_list
+
+
+def add_product(list, **kwards):
+    current_id = 0
+    for item in list:
+        if (item["_id"] > current_id):
+            current_id = item["_id"]
+    new_Product = {"_id": current_id+1, **kwards}
+    list.append(new_Product)
+    return new_Product
